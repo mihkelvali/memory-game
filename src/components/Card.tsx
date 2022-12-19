0e4908d743eb080bf590c2342e0e6c5e7f106a72
Mihkel Väli <mihkel.vali@hotmail.com>
2022-12-19 15:53:13 +0200

    Single player some-what working

---
+++ src/components/Card.tsx
@@ -0,0 +1,44 @@
+import { FC } from "react";
+import Image from "next/image";
+import cardStyles from "../styles/Card.module.css";
+
+type Props = {
+  id: number,
+  imageName: string,
+  isVisible: boolean,
+  isFaceUp: boolean,
+  onClick: (id: number) => void,
+}
+
+const Card: FC<Props> = ({ id, imageName, isVisible, isFaceUp, onClick }) => {
+  return (
+    <div
+      onClick={() => {
+        onClick(id);
+      }}
+      className={`${cardStyles.card} ${!isVisible ? cardStyles.hidden : ''}`}
+    >
+      <div className={`${cardStyles.cardInner} ${isFaceUp ? cardStyles.turned : ''}`}>
+        <div className={cardStyles.background}>
+          <Image
+            src="/assets/images/background.jpeg"
+            alt="Card"
+            layout="fill"
+            objectFit="cover"
+            priority
+          />
+        </div>
+        <div className={cardStyles.foreground}>
+          <Image
+            src={`/assets/images/${imageName}`}
+            alt="Card"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
--- src/pages/new/single.tsx
+++ src/pages/new/single.tsx
@@ -1,55 +1,116 @@
-import { useState } from "react";
-import { NextPage, NextComponentType } from "next";
+import { NextPage } from "next";
 import Head from "next/head";
+import { useImmer } from "use-immer";
+import { useEffect, useState } from "react";
+import Card from "../../components/Card";
 import Header from "../../components/Header";
 import styles from "../../styles/NewSingle.module.css";
-import cardStyles from "../../styles/Card.module.css";
-import Image from "next/image";
 
-const Card: any = () => {
-  const [isTurned, setIsTurned] = useState(false);
-  const [imageName, setImageName] = useState('001.png');
-
-  return (
-    <div
-      onClick={() => {
-        setIsTurned((prevState) => !prevState)
-      }}
-      className={cardStyles.card}
-    >
-      <div className={`${cardStyles.cardInner} ${isTurned ? cardStyles.turned : ''}`}>
-        <div className={cardStyles.background}>
-          <Image
-            src="/assets/images/background.jpeg"
-            alt="Card"
-            layout="fill"
-            objectFit="cover"
-            priority
-          />
-        </div>
-        <div className={cardStyles.foreground}>
-          <Image
-            src={`/assets/images/${imageName}`}
-            alt="Card"
-            layout="fill"
-            objectFit="contain"
-          />
-        </div>
-      </div>
-    </div>
-  )
+enum CardSide {
+  FACE_UP, FACE_DOWN
 }
 
 const NewGame: NextPage = () => {
+  const [game, setGame] = useImmer({
+    0: { image: '001', isFaceUp: false, isVisible: true, matchingId: 1 },
+    1: { image: '001', isFaceUp: false, isVisible: true, matchingId: 0 },
+    2: { image: '004', isFaceUp: false, isVisible: true, matchingId: 3 },
+    3: { image: '004', isFaceUp: false, isVisible: true, matchingId: 2 },
+    4: { image: '007', isFaceUp: false, isVisible: true, matchingId: 5 },
+    5: { image: '007', isFaceUp: false, isVisible: true, matchingId: 4 },
+  });
+  const [turnedCards, setTurnedCards] = useImmer([]);
+  const [madeMoves, setMadeMoves] = useImmer(0);
+  let timer: NodeJS.Timeout;
+
+  const onClick = (id: Number): void => {
+    turnCard(id, CardSide.FACE_UP);
+    setTurnedCards((prev): any => {
+      if (!turnedCards.includes(id)) {
+        prev.push(id);
+      }
+    });
+  }
+
+  const checkGameStatus = (): void => {
+    console.log(turnedCards)
+    if (turnedCards.length < 2) {
+      console.log('turn another card over ', turnedCards);
+      return;
+    }
+    setMadeMoves((prev) => ++prev);
+    setTimeout(() => {
+      setTurnedCards([]);
+      if (isMatchOnTable()) {
+        console.log('found a match!', turnedCards);
+        hideCards(turnedCards);
+        return;
+      }
+      console.log('unlucky, try again!');
+      turnCardsFaceUp();
+    }, 1000);
+    return;
+  };
+
+  const turnCardsFaceUp = (): void => {
+    for (const cardId of turnedCards) {
+      turnCard(cardId, CardSide.FACE_DOWN);
+    }
+  };
+
+  const isMatchOnTable = (): boolean => {
+    for (const cardId of turnedCards) {
+      if (turnedCards.includes(game[String(cardId)].matchingId)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  const turnCard = (id: Number, cardSide: CardSide): void => {
+    setGame((draftGame) => {
+      draftGame[String(id)].isFaceUp = cardSide === CardSide.FACE_UP;
+    });
+    if (cardSide === CardSide.FACE_DOWN) {
+      setTurnedCards((prev) => prev.filter(cardId => cardId !== id));
+    }
+  };
+
+  const hideCards = (cardIds: number[]): void => {
+    for (const cardId of cardIds) {
+      setGame((draftGame) => {
+        draftGame[String(cardId)].isVisible = false;
+      });
+    }
+  }
+
+  useEffect(() => {
+    checkGameStatus();
+  }, [turnedCards]);
+
+  useEffect(() => {
+    return () => clearTimeout(timer);
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>New game | Memory game</title>
+        <title>Single-player game | Memory game</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <span>Moves made: {madeMoves}</span>
       <div className={styles.main}>
-        <Card />
+        {Object.entries(game).map(([id, card]) => (
+          <Card
+            key={id}
+            id={Number(id)}
+            imageName={`${card.image}.png`}
+            isVisible={card.isVisible}
+            isFaceUp={card.isFaceUp}
+            onClick={onClick}
+          />)
+        )}
       </div>
     </div>
   );
